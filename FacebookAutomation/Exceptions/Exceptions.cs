0bdbb8a206a86cae21a005695c21202a22101b5a
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2025-01-28 15:24:36 +0200

    Add: enhance deserializing request to catch the new dtsg token

---
+++ FacebookAutomation/Exceptions/Exceptions.cs
@@ -0,0 +1,6 @@
+﻿namespace FacebookAutomation.Exceptions
+{
+    public class NewDtsgTokenException : Exception
+    {
+    }
+}
--- FacebookAutomation/Utils/Helper.cs
+++ FacebookAutomation/Utils/Helper.cs
@@ -1,4 +1,5 @@
-﻿using FacebookAutomation.Models.Facebook;
+﻿using FacebookAutomation.Exceptions;
+using FacebookAutomation.Models.Facebook;
 using Newtonsoft.Json.Linq;
 
 namespace FacebookAutomation.Utils
@@ -10,12 +11,38 @@ namespace FacebookAutomation.Utils
             return nextPage?.End_Cursor != null && nextPage.Has_Next_Page ? $"\"{nextPage.End_Cursor}\"" : "null";
         }
 
+        public static JObject DeserializeJson(string content)
+        {
+            string[] lines = content.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            string firstJsonObject = lines[0].Trim();
+            return JObject.Parse(firstJsonObject);
+        }
+
         public static async Task<JObject> DeserializeResponseToDynamic(HttpResponseMessage response)
         {
             var responseContent = await response.Content.ReadAsStringAsync();
-            string[] lines = responseContent.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
-            string firstJsonObject = lines[0].Trim();
-            return JObject.Parse(firstJsonObject);
+
+            if (string.IsNullOrEmpty(responseContent))
+                return new JObject();
+
+            // Regular expression to match the JavaScript code pattern (e.g., for (;;);{...})
+            var regex = new System.Text.RegularExpressions.Regex(@"^\s*for\s*\(\s*;;\s*\);\s*(\{.*\})", System.Text.RegularExpressions.RegexOptions.Singleline);
+            var match = regex.Match(responseContent);
+
+            // If a match is found, extract the JSON part
+            if (match.Success)
+            {
+                var jsonObject = JObject.Parse(match.Groups[1].Value.Trim());
+                var fb_dtsg = jsonObject["dtsgToken"]?.ToString();
+
+                if (!string.IsNullOrEmpty(fb_dtsg))
+                {
+                    FormDataState.Instance.SetFormData("fb_dtsg", fb_dtsg);
+                    throw new NewDtsgTokenException();
+                }
+            }
+
+            return DeserializeJson(responseContent);
         }
 
         public static async Task<string> ExtractJsonFromJavascript(HttpResponseMessage response)
