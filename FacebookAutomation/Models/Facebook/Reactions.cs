3429c4695a2cdbc8c8f0fc1702f2d150a4a071d4
Abdelrahman  Fawzy <abdelrahmanfawzy003@gmail.com>
2025-02-01 01:05:04 +0200

    Add: add two features to fetch data from a given page and to add feedback on the posts of a given page

--- FacebookAutomation/Models/Facebook/Reactions.cs
+++ FacebookAutomation/Models/Facebook/Reactions.cs
@@ -9,5 +9,31 @@
         public const string HAHA = "115940658764963";
         public const string WOW = "478547315650144";
         public const string ANGRY = "444813342392137";
+
+        public static string GetReactionId(ReactionsEnum? reaction)
+        {
+            return reaction switch
+            {
+                ReactionsEnum.LIKE => LIKE,
+                ReactionsEnum.LOVE => LOVE,
+                ReactionsEnum.CARE => CARE,
+                ReactionsEnum.SAD => SAD,
+                ReactionsEnum.HAHA => HAHA,
+                ReactionsEnum.WOW => WOW,
+                ReactionsEnum.ANGRY => ANGRY,
+                _ => ""
+            };
+        }
+    }
+
+    public enum ReactionsEnum
+    {
+        LIKE,
+        LOVE,
+        CARE,
+        SAD,
+        HAHA,
+        WOW,
+        ANGRY
     }
 }
